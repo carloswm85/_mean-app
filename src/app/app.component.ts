e7880849af1f4a278b708f719244d6b487eeb326
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-07-06 08:28:34 -0300

    update # databinding {{ }}, [ ], ( ), [( )]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostCreateComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'mean-app';
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './post-create.component.html',
-  styleUrl: './post-create.component.css'
+  styleUrl: './post-create.component.css',
 })
 export class PostCreateComponent {
+  enteredValue1 = 'CONTENT 1';
+  enteredValue2 = 'CONTENT 2';
+  newPost1 = 'Empty 1';
+  newPost2 = 'Empty 2';
 
+  onAddPost1(postInput: HTMLTextAreaElement) {
+    this.newPost1 = postInput.value;
+  }
+  onAddPost2() {
+    this.newPost2 = this.enteredValue2;
+  }
 }
