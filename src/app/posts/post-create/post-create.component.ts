6a1c6e647cb3d5f0431c3e645031fcea0b999957
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-07-06 11:26:57 -0300

    add # header and post create components

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { MaterialModule } from '../../material/material.module';
+
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MaterialModule],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css',
 })
